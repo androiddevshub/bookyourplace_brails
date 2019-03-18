6e7b98cb4bc01bfa896e097975b5811f1bf950f6
Shubham Jain <shubjain440@gmail.com>
2019-03-19 02:13:43 +0530

    Add mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "devilshub7@gmail.com",
+    password: "ShubJain@1995",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  
 end

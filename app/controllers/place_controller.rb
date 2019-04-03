699b3dffec0bf2a4c017606706a40c46c6df4190
Shubham Jain <shubjain440@gmail.com>
2019-04-04 04:36:49 +0530

    changes

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -3,9 +3,9 @@ class PlaceController < ApplicationController
   def show
     id = User.find_by(session_id: request.headers['session-id'])
     if id.present?
-      render json: { data: Place.all }
+      render json: { data: Place.all }, status: :ok
     else
-      render json: { data: 'Invalid session id' }
+      render json: { errors: 'Invalid session id' }, status: :bad_request
     end
   end
 

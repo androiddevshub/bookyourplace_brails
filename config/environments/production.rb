cece4f1f13ee57740ffacd98642f462a5bea91ef
Shubham Jain <shubjain440@gmail.com>
2019-04-04 04:16:30 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     domain: "gmail.com",
     port: 587,
-    user_name: "mathiyugan1267@gmail.com",
-    password: "Androidproject2019",
+    user_name: "devilshub7@gmail.com",
+    password: "ShubJain@1995",
     authentication: 'plain',
     enable_starttls_auto: true
   }

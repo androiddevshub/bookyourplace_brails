be9240108e9475162cdd24acbfd19a3b2e0cc2d6
Shubham Jain <shubjain440@gmail.com>
2019-04-05 15:08:19 +0530

    Changes

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -36,4 +36,15 @@ class VerifyController < ApplicationController
     end
   end
 
+  def user_logged_in
+    user = User.find_by(session_id: request.headers['session-id'])
+    if user.present?
+      render json: { message: 'Logged in', code: 1 }, status: :ok
+    else
+      render json: { errors: 'Invalid session id' }, status: :bad_request
+    end
+  end
+
+
+
 end
